009595ef45cf5eef67b84561e2fa179816e085c7
Anhgelus Morhtuuzh <william@herges.fr>
2026-09-04 23:08:37 +0200

    build(gradle): bump versions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "world.anhgelus.parismobility"
         minSdk = 28
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
